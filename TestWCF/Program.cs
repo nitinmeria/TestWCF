c277f0874f1e39278626e9791843e72fb3aaebe9
nitinmeria <meria.nitin@gmail.com>
2015-02-14 11:56:47 +0530

    Initial Commit

---
+++ TestWCF/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestWCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WSSRManageWebAccount.ManageWebAccountServiceClient proxy = new WSSRManageWebAccount.ManageWebAccountServiceClient();
+            WSSRManageWebAccount.SupporterProfileDetails profile = new WSSRManageWebAccount.SupporterProfileDetails();
+            profile.email = "asdasasd";
+            WSSRManageWebAccount.SupporterProfileDetailsReponse response = new WSSRManageWebAccount.SupporterProfileDetailsReponse();
+            response = proxy.UpdateSupporterProfileDetails(profile);
+        }
+    }
+}
